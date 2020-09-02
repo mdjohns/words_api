39808cbc724347f660120f232d18729989e50c13
Dylan Johnson <mdjohnson2@ualr.edu>
2020-09-02 17:39:51 -0500

    completed random function

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 5000;
+
+//TODO: if we're feeling crazy, cache found words to avoid duplicates >:)
+
+
+function getWordsFromFile(numWords, filePath) {
+    const file = fs.readFileSync(filePath);
+    const parsed = JSON.parse(file);
+    const len = parsed.length;
+    let results = new Array(numWords);
+
+    while (numWords--) {
+        let x = Math.floor(Math.random() * len);
+        results[numWords] = parsed[x];
+    }
+
+    return results;
+}
+// Without query params return 1 word
+
+app.get('/word', async (req, res) => {
+    let numWords = 0;
+    req.query.n ? numWords = req.query.n : numWords = 1
+    const resp = getWordsFromFile(5, './data/words.json');
+    res.send(resp);
+
+})
+// With query params return n words
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
